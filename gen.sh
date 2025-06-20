4aba8252327b547b51bf0d084078492fbcc5bc27
asn49k <stefanalexnuta@gmail.com>
2025-06-20 13:11:29 +0000

    first global variant

--- gen.sh
+++ gen.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+touch gen_out_corect.txt
+touch gen_out_main.txt
+touch gen_input.txt
 g++ main.cpp -o main.exe
 g++ corect.cpp -o corect.exe
 g++ gen.cpp -o gen.exe
 for((i=1;i<=100;i++))do 
     echo $i
-    ./gen.exe >input.exe
-    ./main.exe <input.txt >out_main.txt
-    ./corect.exe <input.txt >out_corect.txt
-    diff out_main.txt out_corect.txt || break
+    ./gen.exe >gen_input.txt
+    ./main.exe <gen_input.txt >gen_out_main.txt
+    ./corect.exe <gen_input.txt >gen_out_corect.txt
+    diff gen_out_main.txt gen_out_corect.txt || break
 done
+rm -f gen_out_corect.txt gen_out_main.txt gen_input.txt gen.exe main.exe corect.exe
+
