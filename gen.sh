6dfd3194349a3f65700d83b7cd0422c0bc7e97d7
asn49k <stefanalexnuta@gmail.com>
2025-06-21 13:53:41 +0000

    The name of the files and the number of tests can be specified

--- gen.sh
+++ gen.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+MAIN_FILE=${1:-main.cpp}
+CORRECT_FILE=${2:-corect.cpp}
+GEN_FILE=${3:-gen.cpp}
+nr_teste=${4:-6969}
 touch gen_out_corect.txt
 touch gen_out_main.txt
 touch gen_input.txt
-g++ main.cpp -o main.exe
-g++ corect.cpp -o corect.exe
-g++ gen.cpp -o gen.exe
-for((i=1;i<=100;i++))do 
-    echo $i
+g++ $MAIN_FILE -o main.exe
+g++ $CORRECT_FILE -o corect.exe
+g++ $GEN_FILE -o gen.exe
+#nr_teste=100
+for((i=1;i<=nr_teste;i++))do 
+    echo "Testul numarul: $i"
     ./gen.exe >gen_input.txt
     ./main.exe <gen_input.txt >gen_out_main.txt
     ./corect.exe <gen_input.txt >gen_out_corect.txt
     diff gen_out_main.txt gen_out_corect.txt || break
 done
+if [ "$i" -gt "$nr_teste" ];then
+    echo "A trecut toate testele"
+fi
 rm -f gen_out_corect.txt gen_out_main.txt gen_input.txt gen.exe main.exe corect.exe
 
